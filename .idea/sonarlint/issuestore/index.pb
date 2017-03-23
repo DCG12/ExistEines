93c6649b75466717aec32f389cc6518b9720e540
David <46406163y@iespoblenou.org>
2017-03-23 17:53:37 +0100

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+I
+src/com/company/Main.java,4/a/4a831b10439099d44774d0ccc387e808deffaeab
+O
+src/com/company/XMLDBIntro.java,9/a/9a3668dcf18ba336587ca4e75bb0f2c844f63d90
---
+++ src/com/company/XMLDBIntro.java
@@ -0,0 +1,59 @@
+package com.company;
+
+import org.exist.storage.btree.DBException;
+import org.exist.storage.index.CollectionStore;
+import org.exist.xmldb.CollectionImpl;
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.Database;
+import org.xmldb.api.base.Resource;
+import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.modules.CollectionManagementService;
+
+import java.io.File;
+
+
+public class XMLDBIntro {
+
+    private static String URI = "xmldb:exist://localhost:8080/exist/xmlrpc";
+    private static String driver = "org.exist.xmldb.DatabaseImpl";
+
+    public static void main(String args[]) throws XMLDBException,
+            ClassNotFoundException, IllegalAccessException, InstantiationException{
+
+        try {
+            afegirFitxer("Pokemons.xml");
+        } catch (DBException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void afegirFitxer(String fl) throws XMLDBException,
+            ClassNotFoundException, IllegalAccessException, InstantiationException, DBException {
+        File f = new File("/home/46406163y/Baixades/Factbook.xml");
+
+        // initialize database driver
+        Class cl = Class.forName(driver);
+        Database database = (Database) cl.newInstance();
+        database.setProperty("create-database", "true");
+
+        // crear el manegador
+        DatabaseManager.registerDatabase(database);
+
+        // adquirir la col·lecció que volem tractar
+        Collection col = DatabaseManager.getCollection(URI+"/db","admin","admin");
+
+        //Creem la col·lecció on guardarem el recurs
+        CollectionManagementService colmgt = (CollectionManagementService) col.getService("CollectionManagementService", "1.0");
+
+        col = colmgt.createCollection("Recursos");
+
+
+        //afegir el recurs que farem servir
+        Resource res = col.createResource("pokemons.xml","XMLResource");
+        res.setContent(f);
+        col.storeResource(res);
+
+    }
+}
