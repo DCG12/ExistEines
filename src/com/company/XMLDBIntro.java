b656106060dfd35694597f2f9026ea8f332ddf87
David <46406163y@iespoblenou.org>
2017-03-23 19:10:08 +0100

    Resuelto hasta la tercera question

--- src/com/company/XMLDBIntro.java
+++ src/com/company/XMLDBIntro.java
@@ -4,11 +4,9 @@ import org.exist.storage.btree.DBException;
 import org.exist.storage.index.CollectionStore;
 import org.exist.xmldb.CollectionImpl;
 import org.xmldb.api.DatabaseManager;
-import org.xmldb.api.base.Collection;
-import org.xmldb.api.base.Database;
-import org.xmldb.api.base.Resource;
-import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.base.*;
 import org.xmldb.api.modules.CollectionManagementService;
+import org.xmldb.api.modules.XQueryService;
 
 import java.io.File;
 
@@ -43,17 +41,22 @@ public class XMLDBIntro {
 
         // adquirir la col·lecció que volem tractar
         Collection col = DatabaseManager.getCollection(URI+"/db","admin","admin");
+        Collection col2 = DatabaseManager.getCollection(URI+"/db","admin","admin");
 
         //Creem la col·lecció on guardarem el recurs
         CollectionManagementService colmgt = (CollectionManagementService) col.getService("CollectionManagementService", "1.0");
 
         col = colmgt.createCollection("Recursos");
 
-
         //afegir el recurs que farem servir
         Resource res = col.createResource("pokemons.xml","XMLResource");
         res.setContent(f);
         col.storeResource(res);
+
+        Resource res2 = col.getResource("pokemons.xml");
+
+
+        System.out.println( res2.getContent() );
 
     }
 }
